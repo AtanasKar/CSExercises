a046f1da0fb30e8576d076b24638b63a7e234f17
Atanas Karaivanov <atanas.kar13@gmail.com>
2025-12-05 14:18:47 +0200

    Add week 5 exercise

---
+++ Week_5/Exercise_1/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Threading;
+using System.Windows;
+
+namespace Exercise_1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            StartButton.IsEnabled = false;
+
+            Thread thread = new Thread(SlowWork);
+            thread.Start();
+        }
+
+        private void SlowWork()
+        {
+            DateTime startTime = DateTime.Now;
+
+            Dispatcher.Invoke(() =>
+            {
+                StartTimeLabel.Content = $"Start Time: {startTime:HH:mm:ss.fff}";
+                NumberLabel.Content = "Number: 0";
+                EndTimeLabel.Content = "End Time: Not finished yet";
+            });
+
+            for (int i = 1; i <= 100; i++)
+            {
+
+                Thread.Sleep(100);
+
+                Dispatcher.Invoke(() =>
+                {
+                    NumberLabel.Content = $"Number: {i}";
+                });
+            }
+
+            DateTime endTime = DateTime.Now;
+
+            Dispatcher.Invoke(() =>
+            {
+                EndTimeLabel.Content = $"End Time: {endTime:HH:mm:ss.fff}";
+                StartButton.IsEnabled = true;
+            });
+        }
+    }
+}
+
